bf7c8891a4626aa2b3b354c6ffa591750a41a0f6
melissa <nadia.mm.mariduena@gmail.com>
2020-08-25 22:58:53 +0200

    allowing the user add data to the list

--- react5-2part/src/components/List.js
+++ react5-2part/src/components/List.js
@@ -11,13 +11,58 @@ export default class List extends React.Component {
   }
   // --------
   render() {
+    //
+    //
+    //
+    // -----------------------------------
+    //
     this.changeHandel1 = (e) => {
+      //console.log(e.target.value);
+      // the console.log will help yoou to see in the browser what you are typing
       this.setState({ userInput: e.target.value.trim() });
+      // console.log(this.state.userInput);
     };
+    // ---this part with the prevent defaul is outside the changeHandel 1 because
+    // it doesnt concern this function anymore.
+    //
+    //
+    //
+    //
+    // -----------------------------------
+    // this area will allow you to add whatever the user types, to the list
+    //------------------------------------
+    //
+    this.add = (e) => {
+      //99% when you are working with forms you need your event (e)
+      // Everytime you work with forms, you will always use:  prevent default
+      //  the prevent default task is to prevent that the form send you to another page when you click on the button.
 
+      e.preventDefault();
+      const userText = this.state.userInput;
+      // after the user type ENTER, i want the useInput empty and then add what the user typed  to the list
+      this.setState({
+        //
+        /* i want to update the userInput with the new value 
+         of it which is nothing: userInput: '',
+         */
+        userInput: "",
+        //
+        // the next step: i want UPDATE THE LIST
+        // to keep the old array which is state list, spread operator is the best friend
+        // as it will allow you to add more data to the already existent list, the new added element will be the: userText
+        list: [...this.state.list, userText],
+      });
+    };
+    //
+    //
+    //
+    // -----------------------------------
+    //
+    //
+    //
     return (
       <React.Fragment>
-        <form>
+        <form onSubmit={this.add}>
           <input
             type="text"
             value={this.state.userInput}
