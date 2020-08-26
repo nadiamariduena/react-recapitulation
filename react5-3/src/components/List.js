f3ede0ef199ed79fda9503b179e62920609d9cc6
melissa <nadia.mm.mariduena@gmail.com>
2020-08-26 23:52:38 +0200

    removing items from the list

--- react5-3/src/components/List.js
+++ react5-3/src/components/List.js
@@ -25,7 +25,7 @@ export default class List extends React.Component {
     //
     //
     // -----------------------------------
-    /*    this area will allow you to add whatever 
+    /* this area will allow you to add whatever 
      the user types, to the list     */
     //------------------------------------
     //
@@ -39,12 +39,45 @@ export default class List extends React.Component {
       });
     };
     //
+    // -----------------------------------
+    /* To REMOVE the item the user clicked on */
+    //------------------------------------
+    //
+    this.remove = (itemToRemove) => {
+      const newArray = this.state.list.filter((item) => item != itemToRemove);
+
+      // the cerise sur le gateau
+      this.setState({
+        list: newArray,
+      });
+
+      /*
+      
+                **  EXPLANATION **
+
+      filter out that specific element the user click, and create
+      a new array out of what last of the list.
+
+       const newArray = this.state.list.filter((item) => item != itemToRemove);
+      
+
+      -------------------- 
+      item != itemToRemove);
+
+      -- IF THE ITEM does not equal to the item to be removed
+      -- i want to create a new array out of the OLD one that 
+      has everything but the one the user click on.
+
+-------------
+
+
+
+      */
+    };
+
     //
     //
     // -----------------------------------
-    //
-    //
-    //
     return (
       <React.Fragment>
         <form onSubmit={this.add}>
@@ -57,7 +90,7 @@ export default class List extends React.Component {
 
           <input type="Submit" value="Add" />
         </form>
-        <UserList list={this.state.list} />
+        <UserList list={this.state.list} del={this.remove} />
       </React.Fragment>
     );
   }
--- react5-3/src/components/UserList.js
+++ react5-3/src/components/UserList.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 const UserList = (props) => {
-  const todos = props.list.map((item, i) => <li key={i}> {item} </li>);
+  const todos = props.list.map((item, i) => (
+    <li key={i}>
+      {item}
+      <i
+        onClick={() => {
+          // console.log(item);
+          props.del(item);
+        }}
+      >
+        X
+      </i>
+    </li>
+  ));
 
   return <ul>{todos}</ul>;
 };
 
 export default UserList;
+
+/**
+the process to remove the item you clicked
+
+
+<i
+        onClick={() => {
+          console.log(item);
+          // props.del(item);
+        }}
+      >
+
+
+        X
+
+
+      </i>
+ */
